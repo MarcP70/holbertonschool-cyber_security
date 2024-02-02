4d6947f60af5524665c5be005553286be6cb5b63
MarcP70 <marcpourias@yahoo.fr>
2024-02-02 15:00:36 +0100

    fix 1-host_header_injection.sh

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST "$2" -d "$3" -w "\n%{redirect_url}\n" | sed 's|http://.*/login|http://'"$1"'/login|'
+curl -X POST "$2" -H "Content-Type: application/x-www-form-urlencoded" -d "$3" | sed 's|http://.*/login|http://'"$1"'/login|'
